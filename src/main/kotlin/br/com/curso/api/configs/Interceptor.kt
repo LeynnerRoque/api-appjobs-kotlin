c58d3096bb213b89ec7a979c0d1f41ee12584b8c
LeynnerRoque <111515763+LeynnerRoque@users.noreply.github.com>
2023-11-17 22:39:50 -0300

    spring doc

--- src/main/kotlin/br/com/curso/api/configs/Interceptor.kt
+++
@@ -1,8 +0,0 @@
-package br.com.curso.api.configs
-
-import org.aopalliance.intercept.Interceptor
-import org.apache.catalina.connector.Response
-import java.net.Authenticator
-
-class Interceptor {
-}
